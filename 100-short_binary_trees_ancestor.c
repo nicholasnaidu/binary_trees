77d482fed173beba9b07b73a0a7e7c270554c4fa
Nicholas Naidu <naidunicholas@gmail.com>
2024-03-28 19:41:15 -0700

    done

---
+++ 100-short_binary_trees_ancestor.c
@@ -0,0 +1,27 @@
+97% of storage used … If you run out, you can't create, edit, and upload files. Get 100 GB of storage for R 34.99 R 8.99/month for 3 months.
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
+ *
+ * Return: If no common ancestors return NULL, else return common ancestor.
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *mom, *pop;
+
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
+}
